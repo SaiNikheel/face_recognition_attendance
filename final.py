898a138bc44b5d06affab55bb55a94f91cf32419
SaiNikheel <95424757+SaiNikheel@users.noreply.github.com>
2025-02-06 12:49:26 +0530

    Update final.py

--- final.py
+++ final.py
@@ -72,7 +72,7 @@ if camera_image:
         st.success(f"Attendance Verified for {verified_name}!")
         st.image(image_path, caption="Matched Image", use_container_width=True)
     else:
-        st.error("User not found in the database!")
+        st.error("User not found in the database! Or No live person is present on the screen")
 
 # Attendance Page
 def attendance_page():
